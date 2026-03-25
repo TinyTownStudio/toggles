4d0f280b172e7cf84b4c2636c4803bfcbcad75cd
reaper <ahoy@barelyhuman.dev>
2026-03-25 09:42:26 +0530

    feat: ground work for templating

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
+import { Home } from "./web/pages/Home.tsx";
 import { Hono } from "hono";
 
 const app = new Hono();
+
+app.get("/",c=>{
+  return c.html(<Home />)
+})
 
 app.get("/health", (c) => {
   return c.json({ status: "ok", timestamp: new Date().toISOString() });
