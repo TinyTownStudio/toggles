047de8285aa713d89a12302e09f5fc7c1dba9acb
reaper <ahoy@barelyhuman.dev>
2026-04-12 19:38:39 +0530

    chore: fix close call

--- packages/app/api/index.ts
+++ packages/app/api/index.ts
@@ -35,7 +35,9 @@ app.use("/api/*", async (c, next) => {
 
   await next();
 
-  connection.$client.close();
+  if ("close" in connection.$client && typeof connection.$client.close === "function") {
+    connection.$client.close();
+  }
 
   c.res.headers.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
   c.res.headers.set("X-Content-Type-Options", "nosniff");
